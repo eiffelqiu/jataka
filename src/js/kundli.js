fa607b65bd1d4e0894124fbe15a324f3c555d6ed
eiffelqiu <eiffelqiu@qq.com>
2020-06-09 15:35:52 +0800

    fix

--- src/js/kundli.js
+++ src/js/kundli.js
@@ -1,4 +1,4 @@
-function draw_gong(canvas,myrashis) {
+function draw_gong(canvas, myrashis) {
     canvas.font = '7pt Arial';
     canvas.textAlign = 'center';
     canvas.fillStyle = 'black';
@@ -6,7 +6,7 @@ function draw_gong(canvas,myrashis) {
     var pos = [125, 93, 62, 43, 50, 54, 111, 104,
         50, 154, 62, 165, 125, 117, 188, 165,
         200, 154, 139, 104, 200, 54, 188, 43];
-   
+
     canvas.fillText(myrashis[0], pos[0], pos[1]);
     canvas.fillText(myrashis[1], pos[2], pos[3]);
     canvas.fillText(myrashis[2], pos[4], pos[5]);
@@ -22,7 +22,7 @@ function draw_gong(canvas,myrashis) {
     canvas.fillText(myrashis[11], pos[22], pos[23]);
 }
 
-function draw_grahas(canvas,mybhavas) {
+function draw_grahas(canvas, mybhavas) {
     canvas.font = '8pt Arial';
     canvas.textAlign = 'center';
     canvas.fillStyle = 'rgb(0,0,255,1)';
@@ -96,104 +96,91 @@ function draw_empty_chart(canvas, canvas_chart, _x, _y) {
 }
 
 function draw_jataka(datao) {
-	generate_house_array(datao,'rasi');
-	var array=[]
-		for(a in datao['dasa']){
-		 array.push(datao['dasa'][a]);
-		}
-		var infoarr = [];
+    generate_house_array(datao, 'rasi');
+    var array = []
+    for (a in datao['dasa']) {
+        array.push(datao['dasa'][a]);
+    }
+    var infoarr = [];
 
-		infoarr.push(['Name',datao['info']['Name']]);
-		infoarr.push(['Place',datao['info']['Place']]);
-        infoarr.push(["lon",datao['info']["lon"]]);
-        infoarr.push(["lat",datao['info']["lat"]]);
-		infoarr.push(["Date Of Birth",datao['info']["DateOfBirth"]]);
-		infoarr.push(["Time Of Birth",datao['info']["TimeOfBirth"]]);
-		infoarr.push(["Langa",datao['info']["Langa"]]);
-		infoarr.push(["Ayanamsa",datao['info']["Ayanamsa"]]);
-		infoarr.push(["Nakshathra",datao['info']["Nakshathra"]['nak']]);
-        infoarr.push(["Nakshathra Pada",datao['info']["Nakshathra"]['pada']]);
-		infoarr.push(["Dasa",datao['info']["Dasa"]]);
-		infoarr.push(["Current Period",datao['info']["CurrentPeriod"]]);
+    infoarr.push(['Name', datao['info']['Name']]);
+    infoarr.push(['Place', datao['info']['Place']]);
+    infoarr.push(["lon", datao['info']["lon"]]);
+    infoarr.push(["lat", datao['info']["lat"]]);
+    infoarr.push(["Date Of Birth", datao['info']["DateOfBirth"]]);
+    infoarr.push(["Time Of Birth", datao['info']["TimeOfBirth"]]);
+    infoarr.push(["Langa", datao['info']["Langa"]]);
+    infoarr.push(["Ayanamsa", datao['info']["Ayanamsa"]]);
+    infoarr.push(["Nakshathra", datao['info']["Nakshathra"]['nak']]);
+    infoarr.push(["Nakshathra Pada", datao['info']["Nakshathra"]['pada']]);
+    infoarr.push(["Dasa", datao['info']["Dasa"]]);
+    infoarr.push(["Current Period", datao['info']["CurrentPeriod"]]);
 
-	    $('#example').DataTable( {
-	        data: infoarr,
-	        "paging":   false,
-	        "ordering": false,
-	        "info":     false,
-	        "searching": false,
-	        columns: [
-	            { title: "" },
-	            { title: "" },
-	        ]
-	    } );
+    $('#example').DataTable({
+        data: infoarr,
+        "paging": false,
+        "ordering": false,
+        "info": false,
+        "searching": false,
+        columns: [
+            { title: "" },
+            { title: "" },
+        ]
+    });
 
-        var infoarr1 = [];
+    var infoarr1 = [];
 
-        var grahas_arr = [];
-        for(a in datao['planets']){
-            var tm = (datao['planets'][a]['character'] == true) ? '(R)':'';
-            grahas_arr.push([datao['planets'][a]['name'] + tm  ,datao['planets'][a]['pos'].toFixed(2),datao['planets'][a]['rasi']]);
+    var grahas_arr = [];
+    for (a in datao['planets']) {
+        var tm = (datao['planets'][a]['character'] == true) ? '(R)' : '';
+        grahas_arr.push([datao['planets'][a]['name'] + tm, datao['planets'][a]['pos'].toFixed(2), datao['planets'][a]['rasi']]);
+    }
+
+    $('#example1').DataTable({
+        data: grahas_arr,
+        "paging": false,
+        "ordering": false,
+        "info": false,
+        "searching": false,
+        columns: [
+            { title: "graha" },
+            { title: "lon" },
+            { title: "rasi" }
+        ]
+    });
+
+    var data = array.sort(function (a, b) { return parseFloat(a.start) - parseFloat(b.start); });
+    var current = '';
+    var d = { title: 'Dasha' };
+    var list = new Array();
+    $.each(data, function (i, item) {
+        var list1 = new Array();
+        var subarray = [];
+
+        if ('current' in item) {
+            // delete item['current'];
+            subarray.push(item.current);
         }
 
-        $('#example1').DataTable( {
-            data: grahas_arr,
-            "paging":   false,
-            "ordering": false,
-            "info":     false,
-            "searching": false,
-            columns: [
-                { title: "graha" },
-                { title: "lon" },
-                { title: "rasi" }
-            ]
-        } );
+        for (a in item.subDasas) {
+            subarray.push(item.subDasas[a]);
+        }
 
-		var data = array.sort(function(a,b){return parseFloat(a.start) - parseFloat(b.start);});
-        var current = '';
-    	var d = {title: 'Dasha'};
-    	var list = new Array();
-        $.each(data, function(i, item){
-            var list1 = new Array();
-            var subarray=[];
-
-            if ('current' in item) {
-            	// delete item['current'];
-            	subarray.push(item.current);
+        var subDasas = subarray.sort(function (a, b) { return parseFloat(a.start) - parseFloat(b.start); });
+        $.each(subDasas, function (j, it) {
+            if ('dasa' in it) {
+                list1.push({ title: it.dasa + ": " + it.startDate })
             }
-
-			for(a in item.subDasas){
-			 	subarray.push(item.subDasas[a]);
-			}
-
-            var subDasas = subarray.sort(function(a,b){return parseFloat(a.start) - parseFloat(b.start);});
-            $.each(subDasas, function(j, it){
-            	if('dasa' in it) {
-            		list1.push({title: it.dasa + ": " + it.startDate})	
-            	}
-            });
-            list.push({title: item.dasa + ": " + item.startDate, nodes: list1})
-
         });
-        d['nodes'] = list;
-        var tree = new TreeDataView({
-	        append: $('#tree-example-3'),
-	        openLevel: 2,
-	    }, d);
-	    tree.render();
+        list.push({ title: item.dasa + ": " + item.startDate, nodes: list1 })
 
-	var dataSet = [
-	    [ "As", 'Aries' ],
-	    [ "Sun", 'Aries' ],
-	    [ "Moon",'Aries' ],
-	    [ "Mars", 'Leo' ],
-	    [ "Rahu", 'Cancer' ],
-	    [ "Jupiter",'Taurus' ],
-	    [ "Saturn", 'Gemini' ],
-	    [ "Mercury", 'Pisces' ],
-	    [ "Ketu", 'Sagittarius' ],
-	    [ "Venus",'Virgo' ],
-	];
+    });
+    d['nodes'] = list;
+    var tree = new TreeDataView({
+        append: $('#tree-example-3'),
+        openLevel: 2,
+    }, d);
+    tree.render();
 }
 
 function draw_d9_chart(canvas, zodiac, house, d) {
@@ -256,72 +243,72 @@ function draw_d9_chart(canvas, zodiac, house, d) {
 
 
 function generate_house_array(obj) {
-	var rashis = {
-		"Mesha": [1,2,3,4,5,6,7,8,9,10,11,12],
-		"Vrishabha":[2,3,4,5,6,7,8,9,10,11,12,1],
-		"Mithuna":[3,4,5,6,7,8,9,10,11,12,1,2],
-		"Kataka":[4,5,6,7,8,9,10,11,12,1,2,3],
-		"Simha":[5,6,7,8,9,10,11,12,1,2,3,4],
-		"Kanya":[6,7,8,9,10,11,12,1,2,3,4,5],
-		"Thula":[7,8,9,10,11,12,1,2,3,4,5,6],
-		"Vrichika":[8,9,10,11,12,1,2,3,4,5,6,7],
-		"Dhanus":[9,10,11,12,1,2,3,4,5,6,7,8],
-		"Makara":[10,11,12,1,2,3,4,5,6,7,8,9],
-		"Kumbha":[11,12,1,2,3,4,5,6,7,8,9,10],
-		"Meena":[12,1,2,3,4,5,6,7,8,9,10,11]
-	};
+    var rashis = {
+        "Mesha": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        "Vrishabha": [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1],
+        "Mithuna": [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2],
+        "Kataka": [4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3],
+        "Simha": [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4],
+        "Kanya": [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5],
+        "Thula": [7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6],
+        "Vrichika": [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7],
+        "Dhanus": [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8],
+        "Makara": [10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        "Kumbha": [11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "Meena": [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    };
 
-	// Rashi
-	var grahas = ['','','','','','','','','','','',''];
-	var rashi = obj['rasi'];
-	var as = [];
+    // Rashi
+    var grahas = ['', '', '', '', '', '', '', '', '', '', '', ''];
+    var rashi = obj['rasi'];
+    var as = [];
 
-	var bava = {};
-	for(key in rashi){
-		if (rashi[key].includes("Ascendant")) {
-			as = rashis[key];
-		}
-	}
-	for(key in rashi){
-		var tmp = "";
-		for(let i = 0; i < rashi[key].length; i++) {
-		  tmp += rashi[key][i].substring(0,2) + " ";
-		}
-		var idx = as.indexOf(rashis[key][0]);
-		grahas[idx] = tmp;
-	}
+    var bava = {};
+    for (key in rashi) {
+        if (rashi[key].includes("Ascendant")) {
+            as = rashis[key];
+        }
+    }
+    for (key in rashi) {
+        var tmp = "";
+        for (let i = 0; i < rashi[key].length; i++) {
+            tmp += rashi[key][i].substring(0, 2) + " ";
+        }
+        var idx = as.indexOf(rashis[key][0]);
+        grahas[idx] = tmp;
+    }
 
-	// D9
+    // D9
 
-	var grahas9 = ['','','','','','','','','','','',''];
-	var rashi9 = obj['navamsa'];
-	var as9 = [];
+    var grahas9 = ['', '', '', '', '', '', '', '', '', '', '', ''];
+    var rashi9 = obj['navamsa'];
+    var as9 = [];
 
-	for(key in rashi9){
-		if (rashi9[key].includes("Ascendant")) {
-			as9 = rashis[key];
-		}
-	}
+    for (key in rashi9) {
+        if (rashi9[key].includes("Ascendant")) {
+            as9 = rashis[key];
+        }
+    }
 
-	for(key in rashi9){
-		var tmp = "";
-		for(let i = 0; i < rashi9[key].length; i++) {
-		  tmp += rashi9[key][i].substring(0,2) + " ";
-		}
-		var idx = as9.indexOf(rashis[key][0]);
-		grahas9[idx] = tmp;
-	}
+    for (key in rashi9) {
+        var tmp = "";
+        for (let i = 0; i < rashi9[key].length; i++) {
+            tmp += rashi9[key][i].substring(0, 2) + " ";
+        }
+        var idx = as9.indexOf(rashis[key][0]);
+        grahas9[idx] = tmp;
+    }
 
-	// drawing
+    // drawing
 
-	var canvas_chart = document.getElementById('canvas_chart');
+    var canvas_chart = document.getElementById('canvas_chart');
     var mychart = canvas_chart.getContext('2d');
     mychart.clearRect(0, 0, canvas_chart.width, canvas_chart.height);
     canvas_chart.onselectstart = function () { return false; }
 
     draw_empty_chart(mychart, canvas_chart, 0, 0);
-    draw_gong(mychart,as);
-    draw_grahas(mychart,grahas);
+    draw_gong(mychart, as);
+    draw_grahas(mychart, grahas);
 
     draw_empty_chart(mychart, canvas_chart, 330, 0);
     draw_d9_chart(mychart, as9, grahas9, '');
